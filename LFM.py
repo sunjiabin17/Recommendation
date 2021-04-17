530d6ca699fedd87b037b9489c83af333207b940
jiabinnn <sunjiabin1995@qq.com>
2021-04-17 10:40:12 +0800

    矩阵分解推荐---隐语义模型

--- LFM.py
+++ LFM.py
@@ -23,6 +23,7 @@ class LFM:
         self.item_ids = set(self.frame['ItemId'].values)
         self.items_dict = {user_id: self.get_pos_neg_item(user_id) for user_id in list(self.user_ids)}
 
+    # 初始化p和q
     def _init_Params(self):
         array_p = np.random.randn(len(self.user_ids), self.class_count)
         array_q = np.random.randn(len(self.item_ids), self.class_count)
@@ -43,6 +44,7 @@ class LFM:
         for item in neg_item_ids: item_dict[item] = 0
         return item_dict
 
+    # p和q点积,然后sigmoid
     def _predict(self, user_id, item_id):
         # 计算user和item之间的兴趣度
         p = np.mat(self.p.loc[user_id].values)
@@ -99,12 +101,83 @@ class LFM:
         self.p, self.q = pickle.load(f)
         f.close()
 
-if __name__ == '__main__':
-    print('init')
-    lfm = LFM()
-    print('train')
-    lfm.train()
-    print('predict')
-    movies = lfm.predict(user_id=1)
-    for movie in movies:
-        print(movie)
+# if __name__ == '__main__':
+#     print('init')
+#     lfm = LFM()
+#     print('train')
+#     lfm.train()
+#     print('predict')
+#     movies = lfm.predict(user_id=1)
+#     for movie in movies:
+#         print(movie)
+
+import torch
+import torch.nn as nn
+import pandas as pd
+import numpy as np
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+class LFM_torch(nn.Module):
+    def __init__(self, n_users, n_items, dim=50):
+        super(LFM_torch, self).__init__()
+        self.users_emb = nn.Embedding(n_users, dim)
+        self.items_emb = nn.Embedding(n_items, dim)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, users_vec, items_vec):
+        users_vec = self.users_emb(users_vec)
+        items_vec = self.items_emb(items_vec)
+        u_dot_i = torch.sum(users_vec*items_vec, dim=1)
+        logit = self.sigmoid(u_dot_i)
+        return logit
+
+# 读文件，返回文件的每一行
+def load_file(filename):
+    with open(filename, 'r') as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                continue
+            yield line.strip('\r\n')
+
+
+def loadData(file_path = 'ml-1m/ratings.dat'):
+    dataset = []
+    userset = set()
+    itemset = set()
+    for line in load_file(file_path):
+        user, item, rating, timestamp = line.split('::')
+        user, item, rating = int(user), int(item), int(rating)
+        userset.add(user)
+        itemset.add(item)
+        dataset.append([user, item, rating])
+    return dataset, max(userset)+1, max(itemset)+1
+
+
+
+def train(epochs=10, batchsize=1024):
+    dataset, n_users, n_items = loadData()
+    print(n_users, " ", n_items)
+    test_ratio = 0.2
+    np.random.shuffle(dataset)
+    # testData = dataset[:int(len(dataset)*test_ratio)]
+    # trainData = dataset[int(len(dataset)*test_ratio):]
+    trainData = dataset
+
+    lfm = LFM_torch(n_users, n_items)
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, lfm.parameters()), lr=0.1)
+    criterion = torch.nn.MSELoss()
+    for epoch in range(epochs):
+        all_loss = 0
+        for user, item, rate in tqdm(DataLoader(trainData, batch_size=batchsize, shuffle=True)):
+            optimizer.zero_grad()
+            r = torch.FloatTensor(rate.detach().numpy())
+            result = lfm(user, item)
+            loss = criterion(result, r)
+            all_loss += loss
+            loss.backward()
+            optimizer.step()
+        print('epoch{}, avg_loss={}'.format(epoch, all_loss / (len(trainData) / batchsize)))
+
+
+train()
